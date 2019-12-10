5aa11ae8fe6a15dacb9e7bbf5f517c01527ca7c1
John Gallaugher <john.gallaugher@bc.edu>
2019-12-10 16:32:44 -0500

    Initial Commit

---
+++ JeopardyTrainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JeopardyTrainer
+//
+//  Created by John Gallaugher on 12/10/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
